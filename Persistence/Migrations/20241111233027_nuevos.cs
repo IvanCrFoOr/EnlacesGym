6270751352be955c51603df3d033a4a16920d352
IvanCrFoOr <69528049+IvanCrFoOr@users.noreply.github.com>
2024-11-11 17:31:30 -0600

    migracion

---
+++ Persistence/Migrations/20241111233027_nuevos.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class nuevos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sugerencia_Places_PlaceId",
+                table: "Sugerencia");
+
+            migrationBuilder.DropTable(
+                name: "PlacesPhotos");
+
+            migrationBuilder.DropTable(
+                name: "Places");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sugerencia_PlaceId",
+                table: "Sugerencia");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Places",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DescriptionPlace = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fsqid = table.Column<string>(name: "Fsq_id", type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Places", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlacesPhotos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlaceId = table.Column<int>(type: "int", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    RowId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrlPhoto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Width = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlacesPhotos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PlacesPhotos_Places_PlaceId",
+                        column: x => x.PlaceId,
+                        principalTable: "Places",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sugerencia_PlaceId",
+                table: "Sugerencia",
+                column: "PlaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlacesPhotos_PlaceId",
+                table: "PlacesPhotos",
+                column: "PlaceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sugerencia_Places_PlaceId",
+                table: "Sugerencia",
+                column: "PlaceId",
+                principalTable: "Places",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
